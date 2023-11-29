2e2e15d360a60d548cff5b6f0d0c3112413e157b
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2023-11-30 00:40:09 +0530

    created an alt way to display it

--- QRCode Generator.py
+++ QRCode Generator.py
@@ -1,5 +1,16 @@
+#DESKTOP VERSION 👇
 import qrcode as qr
 url = input("PASTE any URL(for which you want the QRCODE of): ")
 #url="https://www.youtube.com/watch?v=vmWNRYbc6po&pp=ygUDYTJk"
 pic=qr.make(url)
 pic.save("SHARANQR.png")
+
+
+#CLOUD VERSION 👇
+import qrcode as qr
+from  PIL import  Image
+url=input("Enter  link: ")
+pic=qr.make(url)
+
+#pic.save("photo.png")
+display(pic)
