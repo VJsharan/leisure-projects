00f6caaaa41e7b12dc7b7336b3a365636322b5bd
VijaySharanV <117385197+VJsharan@users.noreply.github.com>
2023-11-30 00:12:30 +0530

    made a few changes to it

--- handcricket.py
+++ handcricket.py
@@ -1,21 +1,29 @@
 import  string
 import  random
 choice=input("Enter  your  choice  (odd/even):  ")
+while(choice!="odd"  and  choice!="even"):
+    print("invalid")
+    choice=input("Enter  your  choice  (odd/even):  ")
 a=int(input("  enter  a  random  number  between  1-6:  "))
+#incase wrong-entry
+while(a>=7):
+    print("invalid  no.")
+    a=int(input("  enter  a  random  number  between  1-6:  "))
 b=random.randint(1,6)
+print(f"computer choice {b}")
 S=a+b
 if(S%2==0 and  choice=="even"):
-  x=input("congrats  you  won  the  toss  \n  what  are  you  choosing  (batting/bowling)??:    ")
+  toss=input("congrats  you  won  the  toss  \n  what  are  you  choosing  (batting/bowling)??:    ")
+  print(f"You  are {toss} first!")
 elif(S%2!=0 and  choice=="odd"):
-  x=input("congrats  you  won  the  toss  \n  what  are  you  choosing  (batting/bowling)??:    ")
+  toss=input("congrats  you  won  the  toss  \n  what  are  you  choosing  (batting/bowling)??:    ")
+  print(f"You   are {toss} first!")
 else:
   print("you  lost  the  toss")
   cric=["batting","bowling"]
   soos=random.choice(cric)
   print(f"The  computer  has  chose  to  {soos}")
   SUM=0
-  
-print(f"You chose {x}")
 for  i  in  range(0,11,1):
   j=int(input("enter  run:  "))
   c=random.randint(1,6)
@@ -24,14 +32,8 @@ for  i  in  range(0,11,1):
     print("golden  duck")
     break
   elif(j==c):
-    SUM+=j
-    print("you got out :(")
-    print(f"You scored {SUM} runs")
+    print("ouch you got out")
+    print(f"you scored {SUM} runs tho!")
     break
   else:
     SUM+=j
-  
-
- 
-  
-  
